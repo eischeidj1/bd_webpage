72863a1065b8d47832161b2f2044a225e8184a38
jacob <jake.eischeid103@gmail.com>
2021-06-28 17:25:35 -0500

    hook-up-database

---
+++ src/main/java/com/example/bd_webpage/Person.java
@@ -0,0 +1,19 @@
+package com.example.bd_webpage;
+
+public class Person {
+
+    private int id;
+    private String first;
+    private String last;
+    private String phone;
+    private String birthday;
+
+    public int getId() {return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getFirst() { return first; }
+    public void setFirst(String first) { this.first = first; }
+
+    /* Add additional getters and setters for each field.
+       Just follow the same pattern. */
+}
