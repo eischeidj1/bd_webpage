423169052139d12597f6ae837aeba0dddcd4d5ef
jacob <jake.eischeid103@gmail.com>
2021-08-03 03:25:57 -0500

    full namelist validation form

--- src/main/webapp/name_list.js
+++ src/main/webapp/name_list.js
@@ -67,15 +67,40 @@ function showDialogAdd() {
     // I'm getting it started, you can finish.
     $('#id').val("");
     $('#first').val("");
+    $('#last').val("");
+    $('#email').val("");
+    $('#phone').val("");
+    $('#birthday').val("");
+
     // Show the hidden dialog
     $('#namelistwindow').modal('show');
+    $('#namelistid').removeClass("is-invalid");
+    $('#namelistid').removeClass("is-valid");
     $('#first').removeClass("is-invalid");
     $('#first').removeClass("is-valid");
-
+    $('#last').removeClass("is-invalid");
+    $('#last').removeClass("is-valid");
+    $('#email').removeClass("is-invalid");
+    $('#email').removeClass("is-valid");
+    $('#phone').removeClass("is-invalid");
+    $('#phone').removeClass("is-valid");
+    $('#birthday').removeClass("is-invalid");
+    $('#birthday').removeClass("is-valid");
 }
 function saveChangesFunction(event) {
     console.log("changes saved");
     // Get the field
+    let id = $('#namelistid').val();
+    let regId = /^[0-9]+$/;
+    let IDisValid = true;
+    if (regId.test(id)){
+        $('#namelistid').removeClass("is-invalid");
+        $('#namelistid').addClass("is-valid");
+    } else {
+        $('#namelistid').addClass("is-invalid");
+        $('#namelistid').removeClass("is-valid");
+        IDisValid = false;
+    }
     let first = $('#first').val();
 
     // Create the regular expression
@@ -89,6 +114,50 @@ function saveChangesFunction(event) {
         $('#first').addClass("is-invalid");
         $('#first').removeClass("is-valid");
         isValid = false;
+    }
+    let last = $('#last').val();
+    let regLast =  /^[A-Za-z]{1,10}$/;
+    let lastValid = true;
+    if (regLast.test(last)){
+        $('#last').removeClass("is-invalid");
+        $('#last').addClass("is-valid");
+    } else{
+        $('#last').addClass("is-invalid");
+        $('#last').removeClass("is-valid");
+        lastValid= false;
+    }
+    let email = $('#email').val();
+    let regEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let regValid = true;
+    if (regEmail.test(email)){
+        $('#email').removeClass("is-invalid");
+        $('#email').addClass("is-valid");
+    } else{
+        $('#email').addClass("is-invalid");
+        $('#email').removeClass("is-valid");
+        regValid = false;
+    }
+    let phone = $('#phone').val();
+    let regPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/;
+    let phoneValid = true;
+    if (regPhone.test(phone)){
+        $('#phone').removeClass("is-invalid");
+        $('#phone').addClass("is-valid");
+    } else {
+        $('#phone').addClass("is-invalid");
+        $('#phone').removeClass("is-valid");
+        phoneValid = false;
+    }
+    let birthday = $('#birthday').val();
+    let regBirthday = /^0[1-9]|1[0-2]\/0[1-9]|1\d|2\d|3[01]\/19|20\d{2}$/;
+    let birthdayValid = true;
+    if (regBirthday.test(birthday)){
+        $('#birthday').removeClass("is-invalid");
+        $('#birthday').addClass("is-valid");
+    } else {
+        $('#birthday').addClass("is-invalid");
+        $('#birthday').removeClass("is-valid");
+        birthdayValid = false;
     }
 }
 // There's a button in the form with the ID "addItem"
